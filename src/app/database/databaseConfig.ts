f3aa35639f46261197f26a2414dae26965110f0f
sneh-soni <ssneh20062003@gmail.com>
2024-01-30 21:16:46 +0530

    Database connection

---
+++ src/app/database/databaseConfig.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URL!);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log("MongoDB Connected Successfully");
+    });
+
+    connection.on("error", (err) => {
+      console.log("MongoDB Connection Error" + err);
+      process.exit();
+    });
+  } catch (error) {
+    console.log("Something Went Wrong");
+    console.log(error);
+  }
+}
